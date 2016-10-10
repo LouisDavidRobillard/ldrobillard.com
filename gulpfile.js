706372a7228fcc3a7446a360a70bba98b35718da
LouisDavidRobillard <louis-david2@hotmail.com>
2016-10-09 22:20:33 -0400

    Added Lateral Menu

--- gulpfile.js
+++ gulpfile.js
@@ -1,9 +1,13 @@
 var gulp = require('gulp');
 var less = require('gulp-less');
+var LessPluginAutoPrefix = require('less-plugin-autoprefix'),
+    autoprefix= new LessPluginAutoPrefix({browsers: ["last 2 versions"]});
 
 gulp.task('less', function() {
     gulp.src('**/*.less')
-        .pipe(less())
+        .pipe(less({
+            plugins:[autoprefix]
+        }))
         .pipe(gulp.dest(function(f) {
             return f.base;
         }))
